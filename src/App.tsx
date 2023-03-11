a41eae80901c55fb5affce26d13181f3acdce11c
Ikenna Porter <ikenna.porter@gmail.com>
2023-03-11 14:23:30 -0500

    Configure api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WelcomeModal from './WelcomeModal/WelcomeModal';
 
 function App() {
   return (
     <div className="App">
+      <WelcomeModal />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/utils.ts
@@ -0,0 +1,36 @@
+
+export const fetchData = (subject: string, numberOfQuestions: string) => {
+    //To make fetch work we need to store the user's input ("subject" and "number of Q/A") in state and access them here
+    //Note: a higher number of questions costs more, so we should limit 'numberOfQuestions' while testing
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          //API KEY:
+          'Authorization': "Bearer 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"
+        },
+        body: JSON.stringify({
+          'model': 'gpt-3.5-turbo',
+          'messages': [
+            {
+                'role':'user', 
+                'content': `Please give me a list of ${parseInt(numberOfQuestions)} ${subject} questions and their answers.`
+            }
+        ],
+          'n': numberOfQuestions,
+          'temperature': 0.5,
+          'top_p': 1,
+          'frequency_penalty': 0,
+          'presence_penalty': 0.5,
+        })
+      };
+
+    fetch('https://api.openai.com/v1/chat/completions', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        //write code to manipulate/store retrieved data here
+        console.log(data)
+  }).catch(err => {
+        console.log(err);
+  });
+  }
