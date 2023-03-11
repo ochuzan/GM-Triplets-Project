d08d56bbff052dfa5e7c5075282f8d98a317c825
Oliver Chuzan <oliverchuzan@Olivers-MacBook-Pro.local>
2023-03-11 16:31:22 -0500

    Set up scoreboard component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import WelcomeModal from './WelcomeModal/WelcomeModal';
 import Board from './components/Board/Board';
+import Scoreboard from './components/Scoreboard/Scoreboard';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <WelcomeModal />
       <Board />
+      <Scoreboard />
     </div>
   );
 }
