b9e03a56109dcf38279d7cf8f912f74ac545fceb
Oliver Chuzan <oliverchuzan@Olivers-MacBook-Pro.local>
2023-03-13 12:17:19 -0400

    Basic scoreboard display no functionality

--- src/components/Scoreboard/Scoreboard.styles.ts
+++ src/components/Scoreboard/Scoreboard.styles.ts
@@ -1,1 +1,27 @@
 import styled from 'styled-components'
+// @import url("https://fonts.googleapis.com/css2?family=Silkscreen&display=swap");
+
+export const ScoreboardContainer = styled.div`
+    margin: 0 5% 0 5%;
+    border-radius: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: aliceblue;
+`;
+
+export const Player = styled.div`
+    font-size: 1.5em;
+`;
+export const PlayerScore = styled.div`
+    /* width: 50%;
+    display:flex;
+    align-items: center;
+    justify-content: center; */
+    text-align: center;
+    width: 70%;
+    margin: 0 auto;
+    border-radius: 25px;
+    background-color: black;
+    color: white;
+`;
--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -1,8 +1,21 @@
+import { PlayerScore, Player, ScoreboardContainer } from "./Scoreboard.styles";
+
 const Scoreboard = () => {
     return (
-        <div>
-            Scoreboard
-        </div>
+        <ScoreboardContainer>
+            <Player>
+                Player 1
+                <PlayerScore>
+                    0
+                </PlayerScore>
+            </Player>
+            <Player>
+                Player 2
+                <PlayerScore>
+                    0
+                </PlayerScore>
+            </Player>
+        </ScoreboardContainer>
     )
 };
 
