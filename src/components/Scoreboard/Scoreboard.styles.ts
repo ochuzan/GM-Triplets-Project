c38909958075abcbb25bae86c14f9ccdd73411fc
Oliver Chuzan <oliverchuzan@Olivers-MacBook-Pro.local>
2023-03-15 11:55:22 -0400

    Update scoreboard styles

--- src/components/Scoreboard/Scoreboard.styles.ts
+++ src/components/Scoreboard/Scoreboard.styles.ts
@@ -1,27 +1,22 @@
-import styled from 'styled-components'
-// @import url("https://fonts.googleapis.com/css2?family=Silkscreen&display=swap");
+import styled from "styled-components";
 
 export const ScoreboardContainer = styled.div`
-    margin: 0 5% 0 5%;
-    border-radius: 25px;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    background-color: aliceblue;
+  margin: 0 5% 0 5%;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: aliceblue;
 `;
 
 export const Player = styled.div`
-    font-size: 1.5em;
+  font-size: 1.5em;
 `;
 export const PlayerScore = styled.div`
-    /* width: 50%;
-    display:flex;
-    align-items: center;
-    justify-content: center; */
-    text-align: center;
-    width: 70%;
-    margin: 0 auto;
-    border-radius: 25px;
-    background-color: black;
-    color: white;
+  text-align: center;
+  width: 70%;
+  margin: 0 auto;
+  border-radius: 25px;
+  background-color: black;
+  color: white;
 `;
