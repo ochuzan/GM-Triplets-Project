3d825da8c561e14d2a64f6492a60a65176d4c809
Ikenna Porter <ikenna.porter@gmail.com>
2023-03-16 15:26:00 -0400

    Add State typing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,66 +6,42 @@ import { createContext, useContext } from 'react';
 import { WelcomeModalWrapper } from './components/WelcomeModal/WelcomeModal.styles';
 import { fetchData } from './utils';
 import QuestionAnswerModal from './components/QuestionAnswerModal/QuestionAnswerModal';
-<<<<<<< HEAD
 import WelcomeModal from './components/WelcomeModal/WelcomeModal';
 
 export interface State {
   isWelcomeModalOpen: boolean;
   setIsWelcomeModalOpen: (boolean: boolean) => void;
-=======
-import { TriviaObj, triviaArr } from './dummydata';
-
-export interface State {
-  isWelcomeModalOpen: boolean;
-  setIsWelcomeModalOpen: any;
-  triviaArr: TriviaObj[];
-  curQuestion: null | TriviaObj;
-  setCurQuestion: any;
->>>>>>> 4b3d74630cf0b12e034ea11bb172059649b34d1b
+  triviaArr: [];
+  curQuestion: string;
+  setCurQuestion: (question: string) => void;
 }
 
 export const AppContext = createContext<State>({
   isWelcomeModalOpen: true,
-<<<<<<< HEAD
-  setIsWelcomeModalOpen: (boolean: boolean) => null
-}) 
-
-function App() {
-  const [isWelcomeModalOpen, setIsWelcomeModalOpen] = useState<boolean>(true);
-  
-  // fetchData("Chemistry", "1");
-
-  return (
-    <AppContext.Provider value={{
-      isWelcomeModalOpen,
-      setIsWelcomeModalOpen,
-=======
-  setIsWelcomeModalOpen: null,
-  triviaArr: triviaArr,
+  setIsWelcomeModalOpen: (boolean: boolean) => null,
+  triviaArr: [triviaArr],
   curQuestion: null,
   setCurQuestion: null
 })
 
 function App() {
-  const [isWelcomeModalOpen, setIsWelcomeModalOpen] = useState(true);
+  const [isWelcomeModalOpen, setIsWelcomeModalOpen] = useState<boolean>(true);
   const [isOpenQuestionModal, setIsOpenQuestionModal] = useState(false);
   const [curQuestion, setCurQuestion] = useState(null);
-  // const handleCloseModal = () => setIsWelcomeModalOpen(false);
-
-  // fetchData("Chemistry", "1");
+  
   useEffect(() => {
 
   }, [curQuestion])
+
   console.log(curQuestion)
 
   return (
     <AppContext.Provider value={{
-      isWelcomeModalOpen: isWelcomeModalOpen,
-      setIsWelcomeModalOpen: setIsWelcomeModalOpen,
+      isWelcomeModalOpen,
+      setIsWelcomeModalOpen,
       triviaArr: triviaArr,
       curQuestion: curQuestion,
       setCurQuestion: setCurQuestion
->>>>>>> 4b3d74630cf0b12e034ea11bb172059649b34d1b
     }}>
       <div className="App">
         <WelcomeModal></WelcomeModal>
