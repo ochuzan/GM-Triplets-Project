f7fce6a9372e68f65c279a917b8b472d654831b2
Ikenna Porter <ikenna.porter@gmail.com>
2023-03-16 13:38:36 -0400

    Start work on welcome modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import WelcomeModal from './WelcomeModal/WelcomeModal';
+import React, { useState } from 'react';
+// import WelcomeModal from './WelcomeModal/WelcomeModal';
 import Board from './components/Board/Board';
+import { createContext, useContext } from 'react';
+import { WelcomeModalWrapper } from './WelcomeModal/WelcomeModal.styles';
+import { fetchData } from './utils';
 
 
 function App() {
+  const [isWelcomeModalOpen, setIsWelcomeModalOpen] = useState(true); 
+  const handleCloseModal = () => setIsWelcomeModalOpen(false);
+
+  const AppContext = createContext({
+    isWelcomeModalOpen: isWelcomeModalOpen,
+    handleCloseModal: handleCloseModal
+  }) 
+
+  fetchData("Chemistry", "1");
+
   return (
-    <div className="App">
-      <WelcomeModal />
-      <Board />
-    </div>
+    <AppContext.Provider value={{
+      isWelcomeModalOpen,
+      handleCloseModal,
+    }}>
+      <div className="App">
+        <Board />
+      </div>
+    </AppContext.Provider>
   );
 }
+
+
 
 export default App;
